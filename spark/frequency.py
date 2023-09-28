af372b2eabcad7858b0ca6ae0e942e5f51eab5ca
JonathanArns <jonathan.arns@googlemail.com>
2023-09-28 13:06:46 +0200

    parse json in spark

--- spark/frequency.py
+++ spark/frequency.py
@@ -1,16 +1,18 @@
-from pyspark.sql import SparkSession #, StructType, StructField, StringType, DoubleType
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 from pyspark.sql.window import Window
+import pyspark.sql.functions as F
 import json
 
-# input_schema = StructType([
-#     StructField("vehicle_id", StringType),
-#     StructField("position", StructType([
-#         StructField("lat", DoubleType),
-#         StructField("long", DoubleType),
-#         StructField("bearing", DoubleType),
-#         StructField("speed", DoubleType),
-#     ])),
-# ])
+input_schema = StructType([
+    StructField("vehicle_id", StringType()),
+    StructField("position", StructType([
+        StructField("lat", DoubleType()),
+        StructField("long", DoubleType()),
+        StructField("bearing", DoubleType()),
+        StructField("speed", DoubleType()),
+    ])),
+])
 
 spark = SparkSession \
     .builder \
@@ -26,10 +28,11 @@ input_df = spark \
     .option("subscribe", "realtime") \
     .load()
 
-# json_df = input_df.select(
-#     # col("key").alias("timestamp"),
-#     from_json(col("value").cast("string"), input_schema).alias("value")
-# )
+json_df = input_df.select(
+    F.col("timestamp"),
+    F.col("key"),
+    F.from_json(F.col("value").cast("string"), input_schema).alias("value")
+)
 
 # row_df = json_df.selectExpr(
 #     # "timestamp",
@@ -42,15 +45,24 @@ input_df = spark \
 
 # window_spec = Window \
 #     .partitionBy("vehicle_id") \
-#     .rowsBetween(-2, 0)
-#     # .orderBy("timestamp")
+#     .rowsBetween(-2, 0) \
+#     .orderBy("timestamp")
 
-# windowed_df = row_df \
-#     .withColumn("frequency", )
+#     # .withColumn("vehicle_id", json_df.selectExpr("value.vehicle_id as vehicle_id")) \
+# prev_time_df = json_df \
+#     .withColumn(
+#         "prev_timestamp",
+#         F.lag(json_df["timestamp"]).over(Window.partitionBy("value.vehicle_id").orderBy("timestamp"))
+#     )
+# time_delta_df = prev_time_df \
+#     .withColumn("time_delta", prev_time_df["timestamp"] - prev_time_df["prev_timestamp"])
 
 # writing a data stream to kafka
-ds = input_df \
-    .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+ds = json_df \
+    .select(
+        F.col("key").cast("string"),
+        F.to_json(F.col("value")).cast("string"),
+    ) \
     .writeStream \
     .format("kafka") \
     .option("checkPointLocation", "/tmp/spark/frequency/checkpoint") \
