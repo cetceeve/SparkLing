af0aa319f1496dd9baf949604ed18487217c8269
JonathanArns <jonathan.arns@googlemail.com>
2023-10-28 15:31:58 +0200

    add rust implementation of API client and metadata join

---
+++ event-engine/src/stream_processor/mod.rs
@@ -0,0 +1,20 @@
+use crate::Vehicle;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+
+mod metadata_join;
+use metadata_join::MetadataJoiner;
+
+pub async fn process_vehicle_stream(
+    mut receiver: UnboundedReceiver<Vehicle>,
+    sender: UnboundedSender<Vehicle>,
+) {
+    let metadata_joiner = MetadataJoiner::init().await;
+    loop {
+        let mut vehicle = receiver.recv().await.expect("broken internal channel");
+
+        // precessing steps
+        metadata_joiner.join_metadata(&mut vehicle);
+
+        sender.send(vehicle).expect("broken internal channel");
+    }
+}
