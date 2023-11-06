6d56637c0338051fb5a9391bb7dc7003233643d6
JonathanArns <jonathan.arns@googlemail.com>
2023-11-06 08:54:48 +0100

    switch out kafka for redis

--- website/server.js
+++ website/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { Kafka } = require('kafkajs')
+const redis = require('redis')
 const app = express()
 const port = 8080
 
-class ConsumerService {
+class SubscriberService {
   constructor() {
     this.observers = []
   }
@@ -20,23 +20,14 @@ class ConsumerService {
     return this;
   }
 
-  async run(consumer) {
-    await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        this.observers.forEach((callback) => callback(message));
-      },
+  async run(redisClient) {
+    await redisClient.subscribe("realtime-with-metadata", (message, channel) => {
+      this.observers.forEach((callback) => callback(message));
     })
   }
 }
 
-// connecting to kafka 
-const kafka = new Kafka({
-  clientId: 'sparkling-app',
-  brokers: ['sparkling-kafka-bootstrap:9092',] // for inside cluster
-  // brokers: ['localhost:9094',] // for outside cluster
-})
-const consumer = kafka.consumer({ groupId: 'sparkling-app' })
-const consumerService = new ConsumerService()
+const subscriberService = new SubscriberService()
 
 app.use('/', express.static('public'))
 
@@ -48,13 +39,13 @@ app.get('/realtime', async (req, res) => {
   res.flushHeaders(); // flush the headers to establish SSE with client
 
   let onMessage = (message) => {
-    res.write('data: '+ message.value.toString() + '\n\n');
+    res.write('data: '+ message.toString() + '\n\n');
   }
-  consumerService.subscribe(onMessage);
+  subscriberService.subscribe(onMessage);
 
   // If client closes connection, stop sending events
   res.on('close', () => {
-      consumerService.unsubscribe(onMessage);
+      subscriberService.unsubscribe(onMessage);
       res.end();
   });
 });
@@ -68,9 +59,10 @@ function sleep(ms) {
 // wait a little until startup
 async function startup() {
   await sleep(5000);
-  await consumer.connect();
-  await consumer.subscribe({topic: "realtime-with-metadata", fromBeginning: false});
-  consumerService.run(consumer)
+  let redisClient = await redis.createClient({
+    url: "redis://sparkling-redis/"
+  }).connect();
+  subscriberService.run(redisClient);
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
