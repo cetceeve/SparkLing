e6b2b1c6a6b5eac424341c0ec280c57fff36a929
Fabian Zeiher <fzeiher@gmail.com>
2023-11-16 22:38:19 +0100

    implemented unbounded sparkling data exporter

---
+++ rt-data-exporter/src/main.rs
@@ -0,0 +1,114 @@
+use serde::{Deserialize, Serialize};
+use redis::{self, PubSubCommands};
+use csv::Writer;
+use tokio::sync::mpsc::{self, UnboundedSender};
+use tokio::time::{self, Duration, Instant};
+use chrono::prelude::*;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Vehicle {
+    pub id: String,
+    pub lng: f32,
+    pub lat: f32,
+    pub timestamp: u64,
+    pub trip_id: Option<String>,
+    pub metadata: Option<VehicleMetadata>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct VehicleMetadata {
+    pub trip_id: String,
+    pub route_type: Option<u64>,
+    pub agency_name: Option<String>,
+    pub route_short_name: Option<String>,
+    pub route_long_name: Option<String>,
+    pub trip_headsign: Option<String>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct CsvSerializableVehicle {
+    pub id: String,
+    pub lng: f32,
+    pub lat: f32,
+    pub timestamp: u64,
+    pub trip_id: Option<String>,
+    pub route_type: Option<u64>,
+    pub agency_name: Option<String>,
+    pub route_short_name: Option<String>,
+    pub route_long_name: Option<String>,
+    pub trip_headsign: Option<String>, 
+}
+
+#[tokio::main]
+async fn main() {
+    // oneshot channel to get data to the writer thread
+    let (sender, mut receiver) = mpsc::unbounded_channel::<String>();
+    tokio::task::spawn(async move {
+       redis_subscriber(sender).await
+    });
+
+    // using tokio sleep with select to create a timeout function
+    const TIMEOUT: u64 = 500;
+    let sleep = time::sleep(Duration::from_millis(TIMEOUT));
+    tokio::pin!(sleep);
+    
+    let mut wtr = Writer::from_path("rt-data.csv").expect("CSV Writer: Could not open file.");
+
+    loop {
+        tokio::select! {
+            Some(msg) = receiver.recv() => {
+                let vehicle: Vehicle = serde_json::from_str(&msg).expect("Serde: Error deserializing vehicle.");
+                let csv_vehicle = make_vehicle_serializable(vehicle);
+                wtr.serialize(csv_vehicle).expect("CSV Writer: Error serializing vehicle to csv.");
+                sleep.as_mut().reset(Instant::now() + Duration::from_millis(TIMEOUT));
+            },
+            // called only when receiver has not received anything for a while
+            () = &mut sleep => {
+                wtr.flush().expect("CSV Writer: Error while flushing to disk");
+                println!("{}: flushed to disc after {}ms of inactivity", Utc::now(), TIMEOUT);
+                sleep.as_mut().reset(Instant::now() + Duration::from_millis(2000));
+            },
+        }
+    }
+}
+
+async fn redis_subscriber(sender: UnboundedSender<String>) {
+    // start redis client
+    let redis_client = redis::Client::open("redis://sparkling-redis/").unwrap();
+    let mut redis_conn = redis_client.get_connection().unwrap();
+
+    let _: () = redis_conn.subscribe(&["realtime-with-metadata"], |msg| {
+        let data: String = msg.get_payload().unwrap();
+        sender.send(data).expect("Internal channel broken");
+        redis::ControlFlow::Continue
+    }).unwrap();
+}
+
+fn make_vehicle_serializable(vehicle: Vehicle) -> CsvSerializableVehicle {
+    match vehicle.metadata {
+       Some(metadata) => CsvSerializableVehicle {
+            id: vehicle.id,
+            lng: vehicle.lng,
+            lat: vehicle.lat,
+            timestamp: vehicle.timestamp,
+            trip_id: vehicle.trip_id,
+            route_type: metadata.route_type, 
+            agency_name: metadata.agency_name,
+            route_short_name: metadata.route_short_name,
+            route_long_name: metadata.route_long_name,
+            trip_headsign: metadata.trip_headsign,
+        },
+        None => CsvSerializableVehicle {
+            id: vehicle.id,
+            lng: vehicle.lng,
+            lat: vehicle.lat,
+            timestamp: vehicle.timestamp,
+            trip_id: vehicle.trip_id,
+            route_type: None,
+            agency_name: None,
+            route_short_name: None,
+            route_long_name: None,
+            trip_headsign: None,
+        }
+    }
+}
