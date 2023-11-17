15a4dfd7bc9ecaacf01f4f47b2784dd7e94bb152
Fabian Zeiher <fzeiher@gmail.com>
2023-11-17 14:10:27 +0100

    split output into files of 1 million entries
    improved error handling

--- rt-data-exporter/src/main.rs
+++ rt-data-exporter/src/main.rs
@@ -1,9 +1,12 @@
-use serde::{Deserialize, Serialize};
-use redis::{self, PubSubCommands};
-use csv::Writer;
-use tokio::sync::mpsc::{self, UnboundedSender};
-use tokio::time::{self, Duration, Instant};
 use chrono::prelude::*;
+use csv::Writer;
+use redis::{self, PubSubCommands};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::sync::Arc;
+use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+use tokio::sync::Mutex;
+use tokio::time::{self, Duration, Instant};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Vehicle {
@@ -36,37 +39,70 @@ pub struct CsvSerializableVehicle {
     pub agency_name: Option<String>,
     pub route_short_name: Option<String>,
     pub route_long_name: Option<String>,
-    pub trip_headsign: Option<String>, 
+    pub trip_headsign: Option<String>,
 }
 
 #[tokio::main]
 async fn main() {
     // oneshot channel to get data to the writer thread
-    let (sender, mut receiver) = mpsc::unbounded_channel::<String>();
-    tokio::task::spawn(async move {
-       redis_subscriber(sender).await
-    });
+    let (sender, receiver) = mpsc::unbounded_channel::<String>();
+    tokio::task::spawn(async move { redis_subscriber(sender).await });
 
+    let guarded_recv = Arc::new(Mutex::new(receiver));
+    loop {
+        let date = Utc::now().format("%Y-%m-%dT%H:%M:%S").to_string();
+        let file_name = format!("{date}-rt-data.csv");
+        match write_single_file(guarded_recv.clone(), file_name.clone(), 1000000).await {
+            Err(_) => {}
+            Ok(()) => println!("Finished writing file: {}", file_name),
+        }
+    }
+}
+
+async fn write_single_file(
+    guarded_receiver: Arc<Mutex<UnboundedReceiver<String>>>,
+    file_name: String,
+    min_rows: u64,
+) -> Result<(), Box<dyn Error>> {
+    let mut receiver = guarded_receiver.lock().await;
     // using tokio sleep with select to create a timeout function
     const TIMEOUT: u64 = 500;
     let sleep = time::sleep(Duration::from_millis(TIMEOUT));
     tokio::pin!(sleep);
-    
-    let mut wtr = Writer::from_path("rt-data.csv").expect("CSV Writer: Could not open file.");
 
+    let mut wtr = Writer::from_path(file_name)?;
+
+    let mut rows_written = 0;
     loop {
         tokio::select! {
             Some(msg) = receiver.recv() => {
-                let vehicle: Vehicle = serde_json::from_str(&msg).expect("Serde: Error deserializing vehicle.");
-                let csv_vehicle = make_vehicle_serializable(vehicle);
-                wtr.serialize(csv_vehicle).expect("CSV Writer: Error serializing vehicle to csv.");
-                sleep.as_mut().reset(Instant::now() + Duration::from_millis(TIMEOUT));
+                if let Ok(vehicle) = serde_json::from_str::<Vehicle>(&msg) {
+                    let csv_vehicle = make_vehicle_serializable(vehicle);
+
+                    match wtr.serialize(csv_vehicle) {
+                        Err(_) => println!("CSV Writer: Error serializing vehicle to csv."),
+                        Ok(()) => {
+                            sleep.as_mut().reset(Instant::now() + Duration::from_millis(TIMEOUT));
+                            rows_written += 1;
+                        },
+                    };
+                } else {
+                    println!("Serde: Error deserializing vehicle.")
+                }
             },
             // called only when receiver has not received anything for a while
             () = &mut sleep => {
-                wtr.flush().expect("CSV Writer: Error while flushing to disk");
-                println!("{}: flushed to disc after {}ms of inactivity", Utc::now(), TIMEOUT);
-                sleep.as_mut().reset(Instant::now() + Duration::from_millis(2000));
+                match wtr.flush() {
+                    Err(_) => println!("CSV Writer: Error while flushing to disk"),
+                    Ok(()) => {
+                        println!("{}: flushed to disc after {}ms of inactivity. Currently {}rows.", Utc::now(), TIMEOUT, rows_written);
+                        sleep.as_mut().reset(Instant::now() + Duration::from_millis(2000));
+
+                        if rows_written > min_rows {
+                            return Ok(());
+                        }
+                    },
+                }
             },
         }
     }
@@ -77,22 +113,26 @@ async fn redis_subscriber(sender: UnboundedSender<String>) {
     let redis_client = redis::Client::open("redis://sparkling-redis/").unwrap();
     let mut redis_conn = redis_client.get_connection().unwrap();
 
-    let _: () = redis_conn.subscribe(&["realtime-with-metadata"], |msg| {
-        let data: String = msg.get_payload().unwrap();
-        sender.send(data).expect("Internal channel broken");
-        redis::ControlFlow::Continue
-    }).unwrap();
+    let _: () = redis_conn
+        .subscribe(&["realtime-with-metadata"], |msg| {
+            match msg.get_payload() {
+                Ok(data) => sender.send(data).expect("Internal channel broken"),
+                Err(_) => println!("Redis: Error retrieving payload from pubsub."),
+            }
+            redis::ControlFlow::Continue
+        })
+        .unwrap();
 }
 
 fn make_vehicle_serializable(vehicle: Vehicle) -> CsvSerializableVehicle {
     match vehicle.metadata {
-       Some(metadata) => CsvSerializableVehicle {
+        Some(metadata) => CsvSerializableVehicle {
             id: vehicle.id,
             lng: vehicle.lng,
             lat: vehicle.lat,
             timestamp: vehicle.timestamp,
             trip_id: vehicle.trip_id,
-            route_type: metadata.route_type, 
+            route_type: metadata.route_type,
             agency_name: metadata.agency_name,
             route_short_name: metadata.route_short_name,
             route_long_name: metadata.route_long_name,
@@ -109,6 +149,6 @@ fn make_vehicle_serializable(vehicle: Vehicle) -> CsvSerializableVehicle {
             route_short_name: None,
             route_long_name: None,
             trip_headsign: None,
-        }
+        },
     }
 }
