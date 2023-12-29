00d5ee0bac5dbbce2871d3b6a99bd0c6b9e4b8d9
JonathanArns <jonathan.arns@googlemail.com>
2023-12-29 09:29:47 +0100

    stream_processor: simplify watermark computation

--- event-engine/src/stream_processor/mod.rs
+++ event-engine/src/stream_processor/mod.rs
@@ -18,21 +18,17 @@ pub trait ProcessingStep: Send {
 pub struct StreamProcessor {
     /// the lowest event timestamp that may appear in following events
     low_watermark: u64,
-    /// used together with processing_time_widow and old_ts_pairs to calculate low_watermark
-    processing_event_times: Vec<(Instant, u64)>,
-    old_ts_pairs: usize,
-    processing_time_widow: Duration,
+    /// used to calculate low_watermark
+    processing_time_widow: u64,
     /// registered processing steps
     processing_steps: Vec<Box<dyn ProcessingStep>>,
 }
 
 impl StreamProcessor {
-    pub fn init(processing_time_widow: Duration) -> Self {
+    pub fn init(processing_time_widow: u64) -> Self {
         Self {
             processing_time_widow,
             low_watermark: 0,
-            processing_event_times: vec![],
-            old_ts_pairs: 0,
             processing_steps: vec![],
         }
     }
@@ -42,7 +38,7 @@ impl StreamProcessor {
     }
 
     pub async fn default() -> Self {
-        let mut processor = Self::init(Duration::from_secs(4));
+        let mut processor = Self::init(5);
         let metadata_joiner = MetadataJoiner::init().await;
         processor.register_step(Box::new(metadata_joiner));
         processor
@@ -56,31 +52,12 @@ impl StreamProcessor {
         'EVENT_LOOP: loop {
             let mut vehicle = receiver.recv().await.expect("broken internal channel");
 
+            // filter old events
+            if vehicle.timestamp < self.low_watermark {
+                continue
+            }
             // update low_watermark
-            self.low_watermark = {
-                let now = Instant::now();
-                let window_barrier = now - self.processing_time_widow;
-                self.processing_event_times.push((now, vehicle.timestamp));
-                let mut recompute = false;
-                while self.processing_event_times[self.old_ts_pairs].0 < window_barrier {
-                    if self.processing_event_times[self.old_ts_pairs].1 == self.low_watermark {
-                        recompute = true;
-                    }
-                    self.old_ts_pairs += 1;
-                }
-                if recompute {
-                    self.processing_event_times.drain(0..self.old_ts_pairs);
-                    let mut new_low = u64::MAX;
-                    for (_, event_time) in &self.processing_event_times {
-                        if *event_time < new_low {
-                            new_low = *event_time;
-                        }
-                    }
-                    new_low
-                } else {
-                    self.low_watermark
-                }
-            };
+            self.low_watermark = self.low_watermark.max(vehicle.timestamp - self.processing_time_widow);
 
             // precessing steps
             for step in &mut self.processing_steps {
