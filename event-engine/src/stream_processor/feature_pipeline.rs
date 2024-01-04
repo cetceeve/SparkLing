c33c3f5d2e5336a788ae5fca782086398c15fad3
JonathanArns <jonathan.arns@googlemail.com>
2024-01-04 03:29:31 +0100

    change feature pipline for sequence classification

--- event-engine/src/stream_processor/feature_pipeline.rs
+++ event-engine/src/stream_processor/feature_pipeline.rs
@@ -1,41 +1,53 @@
 use std::fs::{OpenOptions, File};
+use std::io::Write;
 
 use crate::Vehicle;
 use chrono::{NaiveDateTime, Timelike, Datelike, NaiveTime, Duration};
-use serde::{Deserialize, Serialize};
 
 use super::ProcessingStep;
 
-/// Features for predicting the speed relative to the schedule for the next stops.
-/// This is a sequence to sequence problem.
-/// Input features: ratio of (real time between past stops / scheduled time)
-/// example: [1.2, 1.0, 1.12, 0.78, 1.1]
-/// Output (labels): same ratio, but for future stops
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct TrainingFeatures {
-    // maybe include these
-    // pub lat: f64,
-    // pub lng: f64,
-    pub trip_id: String,
-    pub route_id: String,
-    pub direction_id: u8,
-    pub route_type: u64,
-    /// the day of the week
-    pub weekday: u32,
-    /// the hour of day
-    pub hour: u32,
-    /// the stop_sequence of the startingn stop of the section
-    pub stop_sequence: u32,
-    /// the scheduled section length in seconds
-    pub scheduled_duration: u32,
-    /// the same, but for the next timestep
-    pub next_scheduled_duration: u32,
-    /// the ratio of true duration to scheduled duration
-    pub ratio: f64,
+/// A token in our Vocabulary.
+/// 0-30: time_delta -15m to +15m
+/// 31: sos
+/// 32: eos
+/// 33: pad
+/// 34-47: route_token: route_id + direction_id
+/// 48-149: stop_name ['Kungsträdgården', 'T-Centralen', 'Rådhuset', 'Fridhemsplan', 'Stadshagen', 'Västra skogen', 'Huvudsta', 'Solna strand', 'Sundbybergs centrum', 'Duvbo', 'Rissne', 'Rinkeby', 'Tensta', 'Hjulsta', 'Solna centrum', 'Näckrosen', 'Hallonbergen', 'Kymlinge norrut', 'Kista', 'Husby', 'Akalla', 'Kymlinge söderut', 'Norsborg', 'Hallunda', 'Alby', 'Fittja', 'Masmo', 'Vårby gård', 'Vårberg', 'Skärholmen', 'Sätra', 'Bredäng', 'Mälarhöjden', 'Axelsberg', 'Örnsberg', 'Aspudden', 'Liljeholmen', 'Hornstull', 'Zinkensdamm', 'Mariatorget', 'Slussen', 'Gamla stan', 'Östermalmstorg', 'Karlaplan', 'Gärdet', 'Ropsten', 'Mörby centrum', 'Danderyds sjukhus', 'Bergshamra', 'Universitetet', 'Tekniska högskolan', 'Stadion', 'Fruängen', 'Västertorp', 'Hägerstensåsen', 'Telefonplan', 'Midsommarkransen', 'Skarpnäck', 'Bagarmossen', 'Kärrtorp', 'Björkhagen', 'Hammarbyhöjden', 'Skärmarbrink', 'Gullmarsplan', 'Skanstull', 'Medborgarplatsen', 'Hötorget', 'Rådmansgatan', 'Odenplan', 'S:t Eriksplan', 'Thorildsplan', 'Kristineberg', 'Alvik', 'Stora mossen', 'Abrahamsberg', 'Brommaplan', 'Åkeshov', 'Ängbyplan', 'Islandstorget', 'Blackeberg', 'Råcksta', 'Vällingby', 'Johannelund', 'Hässelby gård', 'Hässelby strand', 'Farsta strand', 'Farsta', 'Hökarängen', 'Gubbängen', 'Tallkrogen', 'Skogskyrkogården', 'Sandsborg', 'Blåsut', 'Hagsätra', 'Rågsved', 'Högdalen', 'Bandhagen', 'Stureby', 'Svedmyra', 'Sockenplan', 'Enskede gård', 'Globen']
+/// 150-156: weekday
+/// 157-180: hour_of_day
+pub struct Token(u64);
+
+/// A complete sequence, used for training of our model
+/// pattern:  sos route_token weekday hour_of_day stop_name time_delta ... stop_name time_delta eos pad pad pad ...
+///
+/// The max trip length in the schedule is 35 stops,
+/// so the max sequence length will be 4 + 35*2 + 1 = 75
+pub struct TrainingSequence(Vec<Token>);
+
+/// Takes input in seconds
+pub fn tokenize_time_delta(delta: i64) -> Token {
+    Token(((delta / 60).max(-15).min(15) + 15) as u64)
+}
+
+pub fn tokenize_route(id: &str, direction_id: u8) -> Token {
+    let routes = vec!["9011001001000000".to_string(), "9011001001100000".to_string(), "9011001001300000".to_string(), "9011001001400000".to_string(), "9011001001700000".to_string(), "9011001001800000".to_string(), "9011001001900000".to_string()];
+    Token(34 + 7 * direction_id as u64 + routes.iter().position(|s| *s == *id).unwrap() as u64)
+}
+
+pub fn tokenize_stop_name(name: &str) -> Token {
+    let stops = vec!["Kungsträdgården".to_string(), "T-Centralen".to_string(), "Rådhuset".to_string(), "Fridhemsplan".to_string(), "Stadshagen".to_string(), "Västra skogen".to_string(), "Huvudsta".to_string(), "Solna strand".to_string(), "Sundbybergs centrum".to_string(), "Duvbo".to_string(), "Rissne".to_string(), "Rinkeby".to_string(), "Tensta".to_string(), "Hjulsta".to_string(), "Solna centrum".to_string(), "Näckrosen".to_string(), "Hallonbergen".to_string(), "Kymlinge norrut".to_string(), "Kista".to_string(), "Husby".to_string(), "Akalla".to_string(), "Kymlinge söderut".to_string(), "Norsborg".to_string(), "Hallunda".to_string(), "Alby".to_string(), "Fittja".to_string(), "Masmo".to_string(), "Vårby gård".to_string(), "Vårberg".to_string(), "Skärholmen".to_string(), "Sätra".to_string(), "Bredäng".to_string(), "Mälarhöjden".to_string(), "Axelsberg".to_string(), "Örnsberg".to_string(), "Aspudden".to_string(), "Liljeholmen".to_string(), "Hornstull".to_string(), "Zinkensdamm".to_string(), "Mariatorget".to_string(), "Slussen".to_string(), "Gamla stan".to_string(), "Östermalmstorg".to_string(), "Karlaplan".to_string(), "Gärdet".to_string(), "Ropsten".to_string(), "Mörby centrum".to_string(), "Danderyds sjukhus".to_string(), "Bergshamra".to_string(), "Universitetet".to_string(), "Tekniska högskolan".to_string(), "Stadion".to_string(), "Fruängen".to_string(), "Västertorp".to_string(), "Hägerstensåsen".to_string(), "Telefonplan".to_string(), "Midsommarkransen".to_string(), "Skarpnäck".to_string(), "Bagarmossen".to_string(), "Kärrtorp".to_string(), "Björkhagen".to_string(), "Hammarbyhöjden".to_string(), "Skärmarbrink".to_string(), "Gullmarsplan".to_string(), "Skanstull".to_string(), "Medborgarplatsen".to_string(), "Hötorget".to_string(), "Rådmansgatan".to_string(), "Odenplan".to_string(), "S:t Eriksplan".to_string(), "Thorildsplan".to_string(), "Kristineberg".to_string(), "Alvik".to_string(), "Stora mossen".to_string(), "Abrahamsberg".to_string(), "Brommaplan".to_string(), "Åkeshov".to_string(), "Ängbyplan".to_string(), "Islandstorget".to_string(), "Blackeberg".to_string(), "Råcksta".to_string(), "Vällingby".to_string(), "Johannelund".to_string(), "Hässelby gård".to_string(), "Hässelby strand".to_string(), "Farsta strand".to_string(), "Farsta".to_string(), "Hökarängen".to_string(), "Gubbängen".to_string(), "Tallkrogen".to_string(), "Skogskyrkogården".to_string(), "Sandsborg".to_string(), "Blåsut".to_string(), "Hagsätra".to_string(), "Rågsved".to_string(), "Högdalen".to_string(), "Bandhagen".to_string(), "Stureby".to_string(), "Svedmyra".to_string(), "Sockenplan".to_string(), "Enskede gård".to_string(), "Globen".to_string()];
+    Token(48 + stops.iter().position(|s| *s == *name).unwrap() as u64)
+}
+
+pub fn tokenize_time(ts: NaiveDateTime) -> (Token, Token) {
+    (
+        Token(150 + ts.weekday().num_days_from_monday() as u64),
+        Token(157 + ts.hour() as u64)
+    )
 }
 
 pub struct TrainingFeatureExtractor {
-    writer: csv::Writer<File>,
+    writer: File,
 }
 
 impl TrainingFeatureExtractor {
@@ -46,8 +58,7 @@ impl TrainingFeatureExtractor {
             .create(true)
             .open("/training-data/training_data.csv")
             .unwrap();
-        let writer = csv::Writer::from_writer(file);
-        Self { writer }
+        Self { writer: file }
     }
 }
 
@@ -55,6 +66,10 @@ impl ProcessingStep for TrainingFeatureExtractor {
     fn apply(&mut self, vehicle: &mut Vehicle, _low_watermark: u64) -> bool {
         // get metadata if there
         if let Some(ref vehicle_metadata) = vehicle.metadata {
+            // we only deal with metros
+            if vehicle_metadata.route_type != Some(401) {
+                return true
+            }
             if let (Some(real_stop_times), Some(stops)) = (&vehicle_metadata.real_stop_times, &vehicle_metadata.stops) {
                 if real_stop_times.len() == 0 || *real_stop_times.last().unwrap() == None {
                     return true // we only generate training features once we reached the last stop
@@ -71,57 +86,29 @@ impl ProcessingStep for TrainingFeatureExtractor {
                 }
                 let scheduled_timestamps = scheduled_times.iter().map(|x| x.timestamp()).collect::<Vec<i64>>();
 
-                // calculate ratios
-                let mut ratios = vec![];
-                let mut scheduled_durations = vec![];
-                let real_stop_times = real_stop_times.iter().map(|x| x.unwrap_or(0)).collect::<Vec<u64>>();
-                let mut i = 1;
-                while i < stops.len() {
-                    if real_stop_times[i-1] == 0 {
-                        ratios.push(1.0);
-                        scheduled_durations.push(scheduled_timestamps[i] - scheduled_timestamps[i-1]);
-                        i += 1;
+                // construct sequence
+                let route_token = tokenize_route(vehicle_metadata.route_id.as_ref().unwrap(), vehicle_metadata.direction_id.unwrap());
+                let (day_token, hour_token) = tokenize_time(scheduled_times[0]);
+                let mut sequence = TrainingSequence(vec![Token(31), route_token, day_token, hour_token]);
+                sequence.0.push(tokenize_stop_name(&stops[0].stop_name));
+                sequence.0.push(tokenize_time_delta(0)); // 0m time_delta
+
+                let mut prev_delay = 0;
+                for (i, stop) in stops[1..].iter().enumerate() {
+                    sequence.0.push(tokenize_stop_name(&stop.stop_name));
+                    if let Some(real_time) = real_stop_times[i] {
+                        let new_delay = real_time as i64 - scheduled_timestamps[i];
+                        sequence.0.push(tokenize_time_delta(new_delay - prev_delay));
+                        prev_delay = new_delay;
                     } else {
-                        let mut j = 0;
-                        while real_stop_times[i+j] == 0 {
-                            j += 1;
-                        }
-                        let mut ratio = (real_stop_times[i+j] as f64 - real_stop_times[i-1] as f64) / (scheduled_timestamps[i+j] as f64 - scheduled_timestamps[i-1] as f64);
-                        if ratio == 0.0 {
-                            ratio = 1.0;
-                        }
-                        if ratio > 50.0 || ratio < 0.0 {
-                            println!("DEBUG: bad ratio: {:?} -> {:?}, {:?} - {:?}, {:?}", ratio, real_stop_times[i+j], real_stop_times[i-1], scheduled_timestamps[i+j], scheduled_timestamps[i-1])
-                        }
-                        let p = i;
-                        for q in 0..=j {
-                            ratios.push(ratio);
-                            scheduled_durations.push(scheduled_timestamps[p+q] - scheduled_timestamps[p+q-1]);
-                            i += 1;
-                        }
+                        sequence.0.push(tokenize_time_delta(0));
                     }
                 }
-                scheduled_durations.push(0);
-                
-                // create multiple training samples from one trip
-                for i in 0..(stops.len()-1) {
-                    let datetime = scheduled_times[i+1].clone();
-                    let features = TrainingFeatures {
-                        // lat: vehicle.lat,
-                        // lng: vehicle.lng,
-                        trip_id: vehicle.trip_id.clone().unwrap(),
-                        route_id: vehicle_metadata.route_id.clone().unwrap(),
-                        direction_id: vehicle_metadata.direction_id.unwrap(),
-                        route_type: vehicle_metadata.route_type.unwrap(),
-                        weekday: datetime.date().weekday().num_days_from_monday(),
-                        hour: datetime.time().hour(),
-                        stop_sequence: i as u32 + 1,
-                        ratio: ratios[i],
-                        scheduled_duration: scheduled_durations[i] as u32,
-                        next_scheduled_duration: scheduled_durations[i+1] as u32,
-                    };
-                    self.writer.serialize(features).unwrap();
+                sequence.0.push(Token(32)); // end of sequence
+                while sequence.0.len() < 75 {
+                    sequence.0.push(Token(33)); // pad to 75
                 }
+                writeln!(&mut self.writer, "{:?}", sequence.0.into_iter().map(|t| t.0).collect::<Vec<u64>>()).unwrap();
                 self.writer.flush().unwrap();
             }
         }
