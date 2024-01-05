6560a95a7a601567b8743472582504b7c672138b
Fabian Zeiher <fzeiher@gmail.com>
2024-01-05 17:20:24 +0100

    added hopsworks feature store and model registry

--- arrival_predictions/lstm-predictions/train.py
+++ arrival_predictions/lstm-predictions/train.py
@@ -4,15 +4,24 @@ import torch.utils.data as tud
 import pytorch_lightning as pl
 import pytorch_lightning.callbacks as plc
 import pytorch_lightning.loggers as pll
+from hsml.schema import Schema
+from hsml.model_schema import ModelSchema
+import joblib
+import os
 
 from model import *
 from dataset import *
+import hopsworks
+
+# connect to feature store
+project = hopsworks.login(project="zeihers_mart")
+fs = project.get_feature_store()
 
 torch.multiprocessing.set_sharing_strategy("file_system")
 torch.manual_seed(0)
 
 # load dataset
-dataset = MetroDelayDataset(data_file="./data/training_data.txt", vocab_file="./data/training_data_vocab.json")
+dataset = MetroDelayDataset(data_file="./data/training_data.txt", vocab_file="./data/training_data_vocab.json", feature_store=fs)
 dataset_train, dataset_val, dataset_test = tud.random_split(dataset, [0.7, 0.15, 0.15])
 print("Vocab size:", dataset.vocab_size)
 print("Training samples:", len(dataset_train))
@@ -75,4 +84,29 @@ trainer.fit(
     val_dataloaders=val_loader,
 )
 
-# trainer.test(model, test_loader)
+metrics = trainer.test(model, test_loader)
+
+mr = project.get_model_registry()
+
+input_schema = Schema(dataset.data[0])
+output_schema = Schema(dataset.data[0])
+model_schema = ModelSchema(input_schema=input_schema, output_schema=output_schema)
+model_schema.to_dict()
+
+model_dir = "torch_tf_model"
+if os.path.isdir(model_dir) == False:
+    os.mkdir(model_dir)
+
+# Save the model
+joblib.dump(model, model_dir + '/metro_delay_lstm.pkl')
+
+# Create a model in the model registry
+model = mr.torch.create_model(
+    name="metro_delay_model",
+    description="PyTorch Lighting LSTM Model predicting the delay at the next stop.",
+    input_example=dataset.data[0],
+    model_schema=model_schema,
+    metrics=metrics[0]
+)
+
+model.save(model_dir)
