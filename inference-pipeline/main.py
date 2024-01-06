7e7acdbd9e60de737b6d1dc84a4258dd58fb759d
Fabian Zeiher <fzeiher@gmail.com>
2024-01-06 19:35:19 +0100

    added delay predictions

--- inference-pipeline/main.py
+++ inference-pipeline/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import joblib
 from io import StringIO
 import numpy as np
+import json
 
 # connect to feature store
 project = hopsworks.login(project="zeihers_mart")
@@ -24,33 +25,21 @@ sub.subscribe("lstm-inference-features")
 
 
 # upload new features to hopsworks
-# buffer_df = None
 for msg in sub.listen():
     if msg["type"] != "message":
         continue
 
     # vehicle_id:<sos>,route,day,hour,stop,delta,stop,delta,stop,pad,stop,pad,stop,pad,pad,
-    training_id, sequence = tuple(msg["data"].split(":"))
-    print(training_id)
-    df = pd.read_csv(StringIO(sequence), header=None)
-    print(df)
-    sequence = df.to_numpy()[0]
-    print(sequence)
-
+    vehicle_id, sequence = tuple(msg["data"].split(":"))
+    sequence = pd.read_csv(StringIO(sequence), header=None).to_numpy()[0]
+    # 31,38,155,174,116,15,115,15,114,15,49,15,89,15,88,15,113,15,112,15,111,15,110,15,109,15,108,15,107,15,106,15,105,15,32,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33
     pred = model.inference(sequence)
-    print(pred)
 
+    delay = 0
+    output = [int(delay)]
+    for token in pred[5::2]: # L[start:stop:step]
+        delay += token - 15
+        output.append(int(delay))
 
+    r.publish("realtime-with-metadata", json.dumps({"id": vehicle_id, "delay": output}))
 
-
-    # if buffer_df is None:
-    #     buffer_df = df
-    # else:
-    #     buffer_df = pd.concat([buffer_df, df])
-
-    # # write to hopsworks if buffer is full
-    # if len(buffer_df) >= 10:
-    #     buffer_df.columns = buffer_df.columns.map(lambda i: f"feature_{i:02d}")
-    #     buffer_df['uuid'] = [str(uuid.uuid4()) for _ in range(len(buffer_df.index))]
-    #     fg.insert(buffer_df)
-    #     buffer_df = None
