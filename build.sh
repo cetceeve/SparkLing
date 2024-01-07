f1e8de626bee250dac37c6341225775d12e25895
Fabian Zeiher <fzeiher@gmail.com>
2024-01-07 15:36:27 +0100

    updated deployment scripts

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@ fi
 
 docker buildx build --platform=$1 -t jonathanarns/sparkling-event-engine ./event-engine
 docker buildx build --platform=$1 -t jonathanarns/sparkling-app ./website
-docker buildx build --platform=$1 -t fabianzeiher/sparkling-rt-data-exporter ./rt-data-exporter
+docker buildx build --platform=$1 -t fabianzeiher/sparkling-inference-pipeline ./inference-pipeline
+docker buildx build --platform=$1 -t fabianzeiher/sparkling-feature-uploader ./feature-uploader
 
 docker push jonathanarns/sparkling-event-engine:latest
 docker push jonathanarns/sparkling-app:latest
-docker push fabianzeiher/sparkling-rt-data-exporter:latest
+docker push fabianzeiher/sparkling-inference-pipeline:latest
+docker push fabianzeiher/sparkling-feature-uploader:latest
