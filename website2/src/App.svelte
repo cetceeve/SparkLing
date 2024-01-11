93d0eb6ec54542b7d527ad219f0d12246fc5b8b3
JonathanArns <jonathan.arns@googlemail.com>
2024-01-11 18:47:45 +0100

    add template vite + svelte app, with still broken OL map component

---
+++ website2/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Map from './Map.svelte';
+</script>
+
+<main>
+ <Map />
+</main>
+
+<style>
+  @import "node_modules/ol/ol.css";
+
+  html,
+  body {
+    margin: 0;
+    height: 100%;
+  }
+</style>
---
+++ website2/src/Map.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { onMount } from 'svelte';
+  import Map from 'ol/Map.js';
+  import OSM from 'ol/source/OSM.js';
+  import TileLayer from 'ol/layer/Tile.js';
+  import View from 'ol/View.js';
+
+  export let map = undefined;
+
+  onMount(() => {
+    map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+  });
+</script>
+
+<div id="map"></div>
+
+<style>
+  @import "node_modules/ol/ol.css";
+
+  #map {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+  }
+</style>
