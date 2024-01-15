7d9e8abbfc57f05ed51fb2aeda055a35b034edbc
JonathanArns <jonathan.arns@googlemail.com>
2024-01-15 19:37:36 +0100

    WIP: WebGL marker on map

--- website2/src/Map.svelte
+++ website2/src/Map.svelte
@@ -1,33 +1,114 @@
 <script>
   import { onMount } from 'svelte';
+
   import "ol/ol.css";
+  import { fromLonLat } from 'ol/proj.js';
   import Map from 'ol/Map.js';
   import OSM from 'ol/source/OSM.js';
-  import TileLayer from 'ol/layer/Tile.js';
+  import VectorSource from 'ol/source/Vector.js';
   import View from 'ol/View.js';
+  import Feature from 'ol/Feature.js';
+  import Point from 'ol/geom/Point.js';
+  import TileLayer from 'ol/layer/Tile.js';
+  import WebGLPointsLayer from 'ol/layer/WebGLPoints.js';
+  import VectorLayer from 'ol/layer/Vector';
 
   export let map = undefined;
+  // export let selectedFeature = null;
 
   onMount(() => {
+    // create the vector source that contains the vehicles to render
+    let vectorSource = new VectorSource({
+      features: [
+        new Feature({
+          type: 'geoMarker',
+          geometry: new Point(fromLonLat([18.071327209472656, 59.34563446044922])),
+        }),
+        new Feature({
+          type: 'geoMarker',
+          geometry: new Point(0,0),
+        }),
+      ],
+    });
+
+    // create the vehicle rendering layer
+    let vehicleLayer = new WebGLPointsLayer({
+      source: vectorSource,
+      // style: {
+
+      //   symbol: {
+      //     symbolType: 'circle',
+      //     size: 15,
+      //     color: 'rgb(255, 0, 0)',
+      //     opacity: 0.7,
+      //   },
+      // },
+      style: {
+        'circle-radius': [
+          'interpolate',
+          ['exponential', 2],
+          ['zoom'],
+          5, // min zoom level
+          2, // min size
+          18, // max zoom level
+          16, // max size
+        ],
+        'circle-fill-color': 'rgb(255, 0, 0)',
+        // 'circle-displacement': [0, 0],
+        'circle-opacity': 1.0,
+      },
+      disableHitDetection: false,
+    });
+
+    // let vehicleLayer = new VectorLayer({
+    //   source: vectorSource,
+    // });
+
+    // create the map
     map = new Map({
       target: 'map-container',
       layers: [
         new TileLayer({
           source: new OSM(),
         }),
+        vehicleLayer,
       ],
       view: new View({
-        center: [0, 0],
-        zoom: 2,
+        center: fromLonLat([18.071327209472656, 59.34563446044922]),
+        zoom: 16,
       }),
     });
+
+
+    // put vehicle features on the layer
+    // vectorSource.addFeatures([
+    //   new Feature({
+    //     type: 'geoMarker',
+    //     geometry: new Point(fromLonLat([18.071327209472656, 59.34563446044922])),
+    //   })
+    // ]);
+
+    // // detect clicked vehicles
+    // // maybe use Selecte({}) interaction instead
+    // map.on('TODO: click', function (ev) {
+    //   if (selectedFeature !== null) {
+    //     selectedFeature.set('selected', false);
+    //     selectedFeature = null;
+    //   }
+
+    //   map.forEachFeatureAtPixel(ev.pixel, function (feature) {
+    //     feature.set('selected', true);
+    //     selectedFeature = feature;
+    //     return true;
+    //   });
+    // });
+
   });
 </script>
 
 <div id="map-container"></div>
 
 <style>
-
   #map-container {
     position: absolute;
     top: 0;
